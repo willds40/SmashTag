09ef9f2ca310186d174197c9c42bdf3e5cd76aac
wdsand <wdsand@thoughtworks.com>
2017-04-11 10:50:14 -0400

    thre sections displaying in second viwcontroller

--- DetailTweetTableViewController.swift
+++ DetailTweetTableViewController.swift
@@ -11,7 +11,14 @@ import Twitter
 
 class DetailTweetTableViewController: UITableViewController {
     //var tweetSelected = Array<Twitter.Tweet>()
-    var mentions = Array<Twitter.Mention>()
+    var hashtags = Array<Twitter.Mention>()
+    var urls = Array<Twitter.Mention>()
+    var userMentions = Array<Twitter.Mention>()
+    var images = Array<Twitter.MediaItem>()
+    var mentions = [Array<Twitter.Mention>]()
+    
+    
+
     var tweetSelected = Array<Twitter.Tweet>()
     
     
@@ -21,27 +28,53 @@ class DetailTweetTableViewController: UITableViewController {
         
         
         
-        for var hashtags in tweetSelected[0].hashtags{
-        addMentions(tweets: [hashtags])
+        for var hashtag in tweetSelected[0].hashtags{
+        hashtags.append(hashtag)
         }
         
-//        for  var urls in tweetSelected[0].urls{
-//        addMentions(tweets: [urls])
-//        }
+        for  var url in tweetSelected[0].urls{
+        urls.append(url)
+        }
+        for  var userMention in tweetSelected[0].urls{
+            userMentions.append(userMention)
+        }
         
-//        for var userMentions in tweetSelected[0].userMentions{
-//        addMentions(tweets: [userMentions])
+//        for  var images in tweetSelected[0].media{
+//            addImages(tweets: [images])
 //        }
+
+        
+        mentions.append(hashtags)
+        mentions.append(urls)
+        mentions.append(userMentions)
+//        mentions.append(images)
+       
 
 }
     
-    private  func addMentions(tweets:Array<Twitter.Mention> ){
-        for mention in tweets {
-            mentions.append(mention)
-        
-        }
+//    private  func addHashtags(tweets:Array<Twitter.Mention> ){
+//        for mention in tweets {
+//                hashtags.append(mention)
+//        }
+//        
+//        }
+//    private  func addUrls(tweets:Array<Twitter.Mention> ){
+//        for mention in tweets {
+//           urls.append(mention)
+//        }
+//            }
+//    private  func addUserMentions(tweets:Array<Twitter.Mention> ){
+//        for mention in tweets {
+//           mentions[2].append(mention)
+//        }
+//           }
+//    private  func addImages(tweets:Array<Twitter.MediaItem> ){
+//        for mention in tweets {
+//            images.append(mention)
+//        }
     
-    }
+//    }
+ 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -52,12 +85,12 @@ class DetailTweetTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return mentions.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of row
-        return mentions.count
+        return mentions[section].count
     }
 
     
@@ -67,8 +100,8 @@ class DetailTweetTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.TweetCellIdentifier, for: indexPath)
-        let mention = mentions[indexPath.row]
-        cell.textLabel?.text = mention.keyword
+      let mention  = mentions[indexPath.section][indexPath.row]
+     cell.textLabel?.text = mention.keyword
 
         return cell
     }
