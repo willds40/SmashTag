caa0e922a658a60efa55e8135fa4dbaae95f4d57
wdsand <wdsand@thoughtworks.com>
2017-04-11 14:49:12 -0400

    added images instead of urls in tweetdetail table view

--- DetailTweetTableViewController.swift
+++ DetailTweetTableViewController.swift
@@ -20,13 +20,13 @@ class DetailTweetTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         for var hashtag in tweetSelected[0].hashtags{
-        hashtags.append(hashtag)
+            hashtags.append(hashtag)
         }
         
         for  var url in tweetSelected[0].urls{
-        urls.append(url)
+            urls.append(url)
         }
         for  var userMention in tweetSelected[0].urls{
             userMentions.append(userMention)
@@ -35,62 +35,63 @@ class DetailTweetTableViewController: UITableViewController {
         for  var image in tweetSelected[0].media{
             images.append(image)
         }
-
+        
         mentions.append(images)
         mentions.append(hashtags)
         mentions.append(urls)
         mentions.append(userMentions)
-}
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return mentions.count
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of row
         return mentions[section].count
     }
-
+    
     
     private struct Storyboard{
-    static let TweetCellIdentifier = "DetailTweet"
+        static let TweetCellIdentifier = "DetailTweet"
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.TweetCellIdentifier, for: indexPath)
-      let mention  = mentions[indexPath.section][indexPath.row]
+        let mention  = mentions[indexPath.section][indexPath.row]
         
         if ((mention as? Twitter.Mention) != nil){
-        cell.textLabel?.text = (mention as! Twitter.Mention).keyword
+            cell.textLabel?.text = (mention as! Twitter.Mention).keyword
         }else{
-//            let pictureURL = (mention as! Twitter.MediaItem).url
-//            let pictureData = NSData(contentsOf: pictureURL as URL)
-//            let catPicture = UIImage(data: pictureData as! Data)
-//            var imageV = UIImageView()
-//            imageV = cell.viewWithTag(1) as! UIImageView
-//            imageV.image = catPicture
-            
-            
-        cell.textLabel?.text = (mention as! Twitter.MediaItem).description
+            let tweetPicture = getPicture(mention: mention)
+            var imageV = UIImageView()
+            imageV = cell.imageView!
+            imageV.image = tweetPicture
         }
-
+        
         return cell
     }
- 
-
+    
+    
+    private func getPicture (mention:Any) -> UIImage{
+        //should be done on the background thread
+        let pictureURL = (mention as! Twitter.MediaItem).url
+        let pictureData = NSData(contentsOf: pictureURL as URL)
+        let tweetPicture = UIImage(data: pictureData as! Data)
+        return tweetPicture!
+    }
     
     
     
-
     
-
-   
+    
+    
 }
