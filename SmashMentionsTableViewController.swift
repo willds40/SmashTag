b51d23b16a3b538e27e3c830ba7fffedf3b9b86e
wdsand <wdsand@thoughtworks.com>
2017-04-20 10:22:47 -0400

    no repeates being added

--- SmashMentionsTableViewController.swift
+++ SmashMentionsTableViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 import CoreData
 
-class SmashMentionsTableViewController: FetchedResultsTableViewController{
+class SmashMentionsTableViewController:
+FetchedResultsTableViewController
+{
     var searchTerm:String?{ didSet{updateUI()}}
     var containter: NSPersistentContainer? =
         (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
     var fetchedResultsController: NSFetchedResultsController<Tweet>?
     var uniqueMentions = [String]()
+    var context = NSManagedObjectContext()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +26,8 @@ class SmashMentionsTableViewController: FetchedResultsTableViewController{
     }
     
     private func updateUI() {
-        if let context = containter?.viewContext,
-            let searchTerm = searchTerm {
+            if let searchTerm = searchTerm {
+            context = (containter?.viewContext)!
             let request: NSFetchRequest<Tweet> = Tweet.fetchRequest()
             request.sortDescriptors = [NSSortDescriptor(
                 key: "unique",
@@ -40,12 +44,17 @@ class SmashMentionsTableViewController: FetchedResultsTableViewController{
             )
             try? fetchedResultsController?.performFetch()
             
-            let arrayOfTweets = fetchedResultsController?.fetchedObjects
-            for tweet in arrayOfTweets!{
+            
+            
+            
+             let arrayOfTweets = (fetchedResultsController?.fetchedObjects)!
+            for tweet in arrayOfTweets{
                 for mention in tweet.mentions!{
-                   uniqueMentions.append((mention as! Mention).uniqueMention!)
+                    uniqueMentions.append((mention as! Mention).uniqueMention!)
                 }
+             uniqueMentions = Array(Set(uniqueMentions))
             }
+            
             tableView.reloadData()
         }
     }
@@ -63,15 +72,22 @@ class SmashMentionsTableViewController: FetchedResultsTableViewController{
         // #warning Incomplete implementation, return the number of row
         return uniqueMentions.count
     }
-
-
+    
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-            let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.MentionPopularityCellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.MentionPopularityCellIdentifier, for: indexPath)
         let menntionCell = uniqueMentions[indexPath.row]
-            cell.textLabel?.text = menntionCell
-        
+        cell.textLabel?.text = menntionCell
+        let mentionCount = mentionCountByCount(menntionCell)
+       
+        cell.detailTextLabel?.text = "\(mentionCount) mention\((mentionCount == 1) ? "" : "s")"
         return cell
     }
-
+    
+    private func mentionCountByCount(_ mention: String) -> Int {
+        let request: NSFetchRequest<Mention> = Mention.fetchRequest()
+        request.predicate = NSPredicate(format: "uniqueMention contains[c] %@", mention)
+        return (try? context.count(for: request)) ?? 0
+    }
 }
